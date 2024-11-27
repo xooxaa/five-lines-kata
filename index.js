455d2a07aaca6b427da1dbdfd14d09d9d1ef86d7
xooxaa <37028661+xooxaa@users.noreply.github.com>
2024-11-27 17:49:00 +0100

    split if-block from handleInputs(); split if-block from updateMap()

--- index.js
+++ index.js
@@ -88,34 +88,40 @@ function update() {
 function handleInputs() {
     while (inputs.length > 0) {
         var current = inputs.pop();
-        if (current === Input.LEFT)
-            moveHorizontal(-1);
-        else if (current === Input.RIGHT)
-            moveHorizontal(1);
-        else if (current === Input.UP)
-            moveVertical(-1);
-        else if (current === Input.DOWN)
-            moveVertical(1);
+        handleInput(current);
     }
+}
+function handleInput(input) {
+    if (input === Input.LEFT)
+        moveHorizontal(-1);
+    else if (input === Input.RIGHT)
+        moveHorizontal(1);
+    else if (input === Input.UP)
+        moveVertical(-1);
+    else if (input === Input.DOWN)
+        moveVertical(1);
 }
 function updateMap() {
     for (var y = map.length - 1; y >= 0; y--) {
         for (var x = 0; x < map[y].length; x++) {
-            if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) && map[y + 1][x] === Tile.AIR) {
-                map[y + 1][x] = Tile.FALLING_STONE;
-                map[y][x] = Tile.AIR;
-            }
-            else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) && map[y + 1][x] === Tile.AIR) {
-                map[y + 1][x] = Tile.FALLING_BOX;
-                map[y][x] = Tile.AIR;
-            }
-            else if (map[y][x] === Tile.FALLING_STONE) {
-                map[y][x] = Tile.STONE;
-            }
-            else if (map[y][x] === Tile.FALLING_BOX) {
-                map[y][x] = Tile.BOX;
-            }
+            updateTile(x, y);
         }
+    }
+}
+function updateTile(x, y) {
+    if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) && map[y + 1][x] === Tile.AIR) {
+        map[y + 1][x] = Tile.FALLING_STONE;
+        map[y][x] = Tile.AIR;
+    }
+    else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) && map[y + 1][x] === Tile.AIR) {
+        map[y + 1][x] = Tile.FALLING_BOX;
+        map[y][x] = Tile.AIR;
+    }
+    else if (map[y][x] === Tile.FALLING_STONE) {
+        map[y][x] = Tile.STONE;
+    }
+    else if (map[y][x] === Tile.FALLING_BOX) {
+        map[y][x] = Tile.BOX;
     }
 }
 function draw() {
