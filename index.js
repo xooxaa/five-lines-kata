dfb3c1bdcd51921722831d595b1efc9ddae70c28
xooxaa <37028661+xooxaa@users.noreply.github.com>
2024-11-27 11:52:44 +0100

    sülit update function into handleInputs and updateMap

--- index.js
+++ index.js
@@ -82,6 +82,10 @@ function moveVertical(dy) {
     }
 }
 function update() {
+    handleInputs();
+    updateMap();
+}
+function handleInputs() {
     while (inputs.length > 0) {
         var current = inputs.pop();
         if (current === Input.LEFT)
@@ -93,6 +97,8 @@ function update() {
         else if (current === Input.DOWN)
             moveVertical(1);
     }
+}
+function updateMap() {
     for (var y = map.length - 1; y >= 0; y--) {
         for (var x = 0; x < map[y].length; x++) {
             if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) && map[y + 1][x] === Tile.AIR) {
